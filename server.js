a75984172a089b17157f287dcdb8561ff32258c3
Justin Vititoe <jvit1985@yahoo.com>
2022-05-09 18:44:53 -0500

    connected to mysql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,24 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MYSQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'Justinis#1!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
